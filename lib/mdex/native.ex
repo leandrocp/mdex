8db87ca42c99b931192966148b9da9b46fa05c2e
Leandro Pereira <leandro@leandro.io>
2023-09-11 14:50:20 -0400

    NIF 2.15 (#2)

--- lib/mdex/native.ex
+++ lib/mdex/native.ex
@@ -4,12 +4,15 @@ defmodule MDEx.Native do
   mix_config = Mix.Project.config()
   version = mix_config[:version]
   github_url = mix_config[:package][:links][:GitHub]
+  mode = if Mix.env() in [:dev, :test], do: :debug, else: :release
 
   use RustlerPrecompiled,
     otp_app: :mdex,
     crate: "comrak_nif",
     base_url: "#{github_url}/releases/download/v#{version}",
     version: version,
+    nif_versions: ["2.15"],
+    mode: mode,
     force_build: System.get_env("MDEX_BUILD") in ["1", "true"]
 
   def to_html(_md), do: :erlang.nif_error(:nif_not_loaded)
