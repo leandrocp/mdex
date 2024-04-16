c2babf9e1b30add79fb449b83bf800ba2a47c369
Leandro Pereira <leandro@leandro.io>
2024-04-16 11:35:01 -0400

    Add target x86_64-unknown-linux-gnux32

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MDEx.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/leandrocp/mdex"
-  @version "0.1.14"
+  @version "0.1.15-dev"
   @dev? String.ends_with?(@version, "-dev")
   @force_build? System.get_env("MDEX_BUILD") in ["1", "true"]
 
