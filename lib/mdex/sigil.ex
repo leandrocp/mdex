a4a4f79e94dcf99c1be60e74d3d83069d73bf9ff
Leandro Pereira <leandro@leandro.io>
2024-10-08 18:45:22 -0400

    Sigils `~M` and `~m` (#74)

--- lib/mdex/sigil.ex
+++ lib/mdex/sigil.ex
@@ -1,58 +1,211 @@
 defmodule MDEx.Sigil do
-  @doc """
-  The `~M` sigil for parsing and formatting Markdown.
+  @opts [
+    extension: [
+      strikethrough: true,
+      table: true,
+      autolink: true,
+      tasklist: true,
+      superscript: true,
+      footnotes: true,
+      description_lists: true,
+      multiline_block_quotes: true,
+      math_dollars: true,
+      math_code: true,
+      shortcodes: true,
+      underline: true,
+      spoiler: true
+    ],
+    parse: [
+      smart: true,
+      relaxed_tasklist_matching: true,
+      relaxed_autolinks: true
+    ],
+    render: [
+      _unsafe: true,
+      escape: true
+    ]
+  ]
 
-  Most options are enabled by default, otherwise use the regular `MDEx.to_html/2` function to customize them for particular cases.
+  @moduledoc """
+  Sigils for parsing and formatting Markdown.
 
   ## Modifiers
 
-    * `a` - calls `MDEx.parse_document!/2` to return the document AST instead of HTML
+    * `AST` - converts Markdown to AST, equivalent to calling `MDEx.parse_document!/2`
+    * `MD` - converts AST to Markdown, equivalent to calling `MDEx.to_commonmark!/2`
+
+  Without a modifier it converts the input (either Markdown or AST) to HTML, equivalent to calling `MDEx.to_html!/2`.
+
+  Note that you should `import MDEx.Sigil` to use the `~M` or `~m` sigils.
+
+  ## Options
+
+  In order to support the most common scenarios, the sigils have the following options enabled by default:
+
+      #{inspect(@opts)}
+
+  If you need a different set of options, you can call the regular functions like `MDEx.to_html/2` to pass the options you need.
+
+  """
+
+  @doc """
+  The `~M` sigil converts between CommonMark, HTML, and AST without interpolation.
 
   ## Examples
 
+      # markdown to html
       iex> ~M|# Hello|
       "<h1>Hello</h1>"
 
-      iex> ~M|# Hello|a
-      [{"document", [], [{"heading", [{"level", 1}, {"setext", false}], ["Hello"]}]}]
+      # markdown to ast
+      iex> ~M|# Hello|AST
+      [{"document", [], [{"heading", [{"level", 1}], ["Hello"]}]}]
 
-  Note that you should `import MDEx.Sigil` to use the `~M` sigil.
+      # ast to markdown
+      iex> ~M|[{"document", [], [{"heading", [{"level", 1}], ["Hello"]}]}]|MD
+      "# Hello"
+
+      # ast to html
+      iex> ~M|[{"document", [], [{"heading", [{"level", 1}], ["Hello"]}]}]|
+      "<h1>Hello</h1>"
+
   """
-  # TODO: deprecated ~M and add ~MD when we start requiring min Elixir 1.15
-  defmacro sigil_M({:<<>>, _meta, [md]}, modifiers) do
-    opts = [
-      extension: [
-        strikethrough: true,
-        table: true,
-        autolink: true,
-        tasklist: true,
-        superscript: true,
-        footnotes: true,
-        description_lists: true,
-        multiline_block_quotes: true,
-        math_dollars: true,
-        math_code: true,
-        shortcodes: true,
-        underline: true,
-        spoiler: true
-      ],
-      parse: [
-        smart: true,
-        relaxed_tasklist_matching: true,
-        relaxed_autolinks: true
-      ],
-      render: [
-        _unsafe: true,
-        escape: true
-      ]
-    ]
-
+  defmacro sigil_M({:<<>>, _meta, [expr]}, modifiers) do
     doc =
       cond do
-        ?a in modifiers -> MDEx.parse_document!(md, opts)
-        :default -> MDEx.to_html!(md, opts)
+        modifiers == ~c"AST" ->
+          MDEx.parse_document!(expr, @opts)
+
+        modifiers == ~c"MD" ->
+          MDEx.Sigil.to_commonmark!(expr)
+
+        # HTML
+        :default ->
+          MDEx.Sigil.to_html!(expr)
       end
 
     Macro.escape(doc)
+  end
+
+  @doc """
+  The `~m` sigil converts between CommonMark, HTML, and AST with interpolation.
+
+  ## Examples
+
+      iex> lang = "elixir"
+
+      # markdown to html
+      iex> ~m|`lang = \#{lang}`|
+      "<p><code>lang = elixir</code></p>"
+
+      # markdown to ast
+      iex> ~m|`lang = \#{lang}`|AST
+      [{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = elixir"}], []}]}]}]
+
+      # ast to markdown
+      iex> ~m[{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = \#{lang}|"}], []}]}]}]|MD
+      "`lang = elixir`"
+
+      # ast to html
+      iex> ~m[{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = \#{lang}|"}], []}]}]}]|
+      "<p><code>lang = elixir</code></p>"
+
+  """
+  defmacro sigil_m({:<<>>, _, [binary]}, modifiers) when is_binary(binary) do
+    cond do
+      modifiers == ~c"AST" ->
+        binary
+        |> Macro.unescape_string()
+        |> MDEx.parse_document!(@opts)
+        |> Macro.escape()
+
+      modifiers == ~c"MD" ->
+        binary = Macro.unescape_string(binary)
+
+        quote do
+          MDEx.Sigil.to_commonmark!(unquote(binary))
+        end
+
+      # HTML
+      :default ->
+        binary
+        |> Macro.unescape_string()
+        |> MDEx.Sigil.to_html!()
+        |> Macro.escape()
+    end
+  end
+
+  defmacro sigil_m({:<<>>, meta, pieces}, modifiers) do
+    binary = {:<<>>, meta, unescape_tokens(pieces)}
+
+    cond do
+      modifiers == ~c"AST" ->
+        quote do
+          MDEx.parse_document!(unquote(binary), unquote(@opts))
+        end
+
+      modifiers == ~c"MD" ->
+        quote do
+          MDEx.Sigil.to_commonmark!(unquote(binary))
+        end
+
+      # HTML
+      :default ->
+        quote do
+          MDEx.Sigil.to_html!(unquote(binary))
+        end
+    end
+  end
+
+  @doc false
+  def to_html!(expr) do
+    with {:ok, [{:{}, _, _}] = quoted} <- Code.string_to_quoted(expr),
+         {ast, _} <- Code.eval_quoted(quoted) do
+      MDEx.to_html!(ast, @opts)
+    else
+      _ -> MDEx.to_html!(expr, @opts)
+    end
+  end
+
+  @doc false
+  def to_commonmark!(expr) do
+    # convert to quoted first before evaluating
+    # so we can check if `expr` is a list of tuples instead of a function call for example
+    case Code.string_to_quoted(expr) do
+      {:ok, [{:{}, _, _}] = quoted} ->
+        {ast, _} = Code.eval_quoted(quoted)
+        MDEx.to_commonmark!(ast, @opts)
+
+      {:ok, quoted} ->
+        other =
+          quoted
+          |> Code.quoted_to_algebra()
+          |> Inspect.Algebra.format(90)
+          |> IO.iodata_to_binary()
+
+        raise """
+        expected a CommonMark AST
+
+        Got:
+
+          #{other}
+
+        """
+
+      error ->
+        error
+    end
+  end
+
+  defp unescape_tokens(tokens) do
+    :lists.map(
+      fn token ->
+        case is_binary(token) do
+          true -> :elixir_interpolation.unescape_string(token)
+          false -> token
+        end
+      end,
+      tokens
+    )
   end
 end
--- test/sigil_test.exs
+++ test/sigil_test.exs
@@ -2,11 +2,67 @@ defmodule MDEx.SigilTest do
   use ExUnit.Case
   import MDEx.Sigil
 
-  test "defaults to HTML" do
-    assert ~M|# Hello| == "<h1>Hello</h1>\n"
+  describe "sigil_M" do
+    test "markdown to html" do
+      assert ~M|`lang = :elixir`| == "<p><code>lang = :elixir</code></p>\n"
+    end
+
+    test "markdown to ast" do
+      assert ~M|`lang = :elixir`|AST == [{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = :elixir"}], []}]}]}]
+    end
+
+    test "ast to html" do
+      assert ~M|[{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = :elixir"}], []}]}]}]| ==
+               "<p><code>lang = :elixir</code></p>\n"
+    end
+
+    test "ast to markdown" do
+      assert ~M|[{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = :elixir"}], []}]}]}]|MD ==
+               "`lang = :elixir`\n"
+    end
   end
 
-  test "AST modifier" do
-    assert ~M|# Hello|a == [{"document", [], [{"heading", [{"level", 1}, {"setext", false}], ["Hello"]}]}]
+  describe "sigil_m without interpolation" do
+    test "markdown to html" do
+      assert ~m|`lang = :elixir`| == "<p><code>lang = :elixir</code></p>\n"
+    end
+
+    test "markdown to ast" do
+      assert ~m|`lang = :elixir`|AST == [{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = :elixir"}], []}]}]}]
+    end
+
+    test "ast to html" do
+      assert ~m|[{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = :elixir"}], []}]}]}]| ==
+               "<p><code>lang = :elixir</code></p>\n"
+    end
+
+    test "ast to markdown" do
+      assert ~m|[{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = :elixir"}], []}]}]}]|MD ==
+               "`lang = :elixir`\n"
+    end
+  end
+
+  describe "sigil_m with interpolation" do
+    @lang :elixir
+
+    test "markdown to html" do
+      assert ~m|`lang = #{inspect(@lang)}`| == "<p><code>lang = :elixir</code></p>\n"
+    end
+
+    test "markdown to ast" do
+      assert ~m|`lang = #{inspect(@lang)}`|AST == [
+               {"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = :elixir"}], []}]}]}
+             ]
+    end
+
+    test "ast to html" do
+      assert ~m|[{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = #{inspect(@lang)}"}], []}]}]}]| ==
+               "<p><code>lang = :elixir</code></p>\n"
+    end
+
+    test "ast to markdown" do
+      assert ~m|[{"document", [], [{"paragraph", [], [{"code", [{"num_backticks", 1}, {"literal", "lang = #{inspect(@lang)}"}], []}]}]}]|MD ==
+               "`lang = :elixir`\n"
+    end
   end
 end
