c24a8aa6862520fedefd7d2376c23a913680487b
Leandro Pereira <leandro@leandro.io>
2024-12-13 19:21:51 -0500

    chore: remove unused code

--- native/comrak_nif/src/types.rs
+++ native/comrak_nif/src/types.rs
@@ -1,18 +1,9 @@
 pub mod document;
-pub mod nodes;
 pub mod options;
 
 pub mod atoms {
     rustler::atoms! {
         ok,
-        error,
-        invalid_structure,
-        empty,
-        missing_node_field,
-        missing_attr_field,
-        node_name_not_string,
-        unknown_node_name,
-        attr_key_not_string,
-        unknown_attr_value,
+        error
     }
 }
