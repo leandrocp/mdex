4fa2b0d879a35d4cd4f1f9035910f4145ab2066c
Leandro Pereira <leandro@leandro.io>
2025-04-05 09:28:08 -0400

    wip: autumnus adapter

--- native/comrak_nif/src/autumnus_adapter.rs
+++ native/comrak_nif/src/autumnus_adapter.rs
@@ -1,5 +1,7 @@
-use autumnus::themes;
+use autumnus::formatter::{HtmlFormatter, HtmlInline, HtmlLinked};
+use autumnus::languages::Language;
 use autumnus::themes::Theme;
+use autumnus::{themes, FormatterOption};
 use comrak::adapters::SyntaxHighlighterAdapter;
 use std::collections::HashMap;
 use std::io::{self, Write};
@@ -7,28 +9,38 @@ use std::io::{self, Write};
 #[derive(Debug)]
 pub struct AutumnusAdapter<'a> {
     theme: &'a Theme,
-    inline_style: bool,
+    formatter_option: FormatterOption<'a>,
 }
 
 impl Default for AutumnusAdapter<'_> {
     fn default() -> Self {
         AutumnusAdapter {
             theme: themes::get("onedark").unwrap(),
-            inline_style: true,
+            formatter_option: FormatterOption::default(),
         }
     }
 }
 
 impl<'a> AutumnusAdapter<'a> {
     pub fn new(theme: &'a str, inline_style: bool) -> Self {
-        let theme = match themes::get(theme) {
+        let theme: &'a Theme = match themes::get(theme) {
             Ok(theme) => theme,
             Err(_) => themes::get("onedark").unwrap(),
         };
 
+        let formatter_option = if inline_style {
+            FormatterOption::HtmlInline {
+                pre_class: None,
+                italic: false,
+                include_highlights: false,
+            }
+        } else {
+            FormatterOption::HtmlLinked { pre_class: None }
+        };
+
         Self {
             theme,
-            inline_style,
+            formatter_option,
         }
     }
 }
@@ -40,15 +52,34 @@ impl SyntaxHighlighterAdapter for AutumnusAdapter<'_> {
         lang: Option<&str>,
         source: &str,
     ) -> io::Result<()> {
+        let lang: Language = Language::guess(lang.unwrap_or(""), source);
+        println!("source: {:?}", source);
         Ok(())
     }
 
     fn write_pre_tag(
         &self,
         output: &mut dyn Write,
-        _attributes: HashMap<String, String>,
+        attributes: HashMap<String, String>,
     ) -> io::Result<()> {
-        write!(output, "{}", "")
+        println!("pre_attributes: {:?}", attributes);
+
+        match self.formatter_option {
+            FormatterOption::HtmlInline { pre_class, .. } => {
+                let formatter = HtmlInline::default()
+                    .with_theme(Some(self.theme))
+                    .with_pre_class(pre_class);
+
+                write!(output, "{}", formatter.pre_tag())
+            }
+            FormatterOption::HtmlLinked { pre_class } => {
+                let formatter = HtmlLinked::default().with_pre_class(pre_class);
+                write!(output, "{}", formatter.pre_tag())
+            }
+            _ => {
+                panic!("Formatter not supported");
+            }
+        }
     }
 
     fn write_code_tag(
@@ -56,6 +87,29 @@ impl SyntaxHighlighterAdapter for AutumnusAdapter<'_> {
         output: &mut dyn Write,
         attributes: HashMap<String, String>,
     ) -> io::Result<()> {
-        write!(output, "{}", "")
+        println!("code_attributes: {:?}", attributes);
+
+        let plaintext = "language-plaintext".to_string();
+        let language = attributes.get("class").unwrap_or(&plaintext);
+        let split: Vec<&str> = language.split('-').collect();
+        let language = split.get(1).unwrap_or(&"plaintext");
+        let lang: Language = Language::guess(language, "");
+
+        match self.formatter_option {
+            FormatterOption::HtmlInline { pre_class, .. } => {
+                let formatter = HtmlInline::default()
+                    .with_theme(Some(self.theme))
+                    .with_lang(lang);
+
+                write!(output, "{}", formatter.code_tag())
+            }
+            FormatterOption::HtmlLinked { pre_class } => {
+                let formatter = HtmlLinked::default().with_lang(lang);
+                write!(output, "{}", formatter.code_tag())
+            }
+            _ => {
+                panic!("Formatter not supported");
+            }
+        }
     }
 }
