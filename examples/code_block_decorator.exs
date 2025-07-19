5d667b38daf194a41ffc2b4111626f8ae535d6e4
Leandro Pereira <leandro@leandro.io>
2025-07-18 20:09:22 -0400

    docs: decorator example

---
+++ examples/code_block_decorator.exs
@@ -0,0 +1,38 @@
+Mix.install([
+  {:mdex, path: ".."}
+])
+
+defmodule CodeBlockDecoratorExample do
+  def run do
+    markdown = """
+    # Code Block Decorator
+
+    ```elixir highlight_lines=2,5,8-10
+    defmodule do
+      @langs [:elixir, :rust]
+
+      def langs do
+        @lang
+      end
+
+      def libs do
+        [:comrak, :ammonia, :autumnus]
+      end
+    end
+    ```
+    """
+
+    html =
+      MDEx.to_html!(markdown,
+        render: [
+          github_pre_lang: true,
+          full_info_string: true
+        ]
+      )
+
+    File.write!("code_block_decorator.html", html)
+    IO.puts(html)
+  end
+end
+
+CodeBlockDecoratorExample.run()
